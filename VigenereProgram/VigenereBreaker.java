0dc674df41412dd292a56cbde002603db0ffa4e5
rachelbookhout <rbookhou@gmail.com>
2017-12-25 21:12:55 -0500

    fixed breakViginere

--- VigenereProgram/VigenereBreaker.java
+++ VigenereProgram/VigenereBreaker.java
@@ -29,13 +29,57 @@ public class VigenereBreaker {
         
        return key;
     }
-
+    public HashSet<String> readDictionary(FileResource fr){
+        HashSet<String> dict = new HashSet<String>();
+        for (String word: fr.words()){
+         dict.add(word.toLowerCase());
+        }
+        return dict;
+    }
+    
+    public int countWords(String encrypted, HashSet<String> dictionary){
+        String[] words = encrypted.toLowerCase().split("\\W+");
+        int count = 0;
+        for(String word: words){
+            if(dictionary.contains(word)){
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    public String breakForLanguage(String encrypted, HashSet<String> dictionary){
+    	HashMap<int[], Integer> keys = new HashMap<int[], Integer>();
+    	System.out.println(encrypted);
+        for(int i= 1; i < 100; i++){
+            int[] key = tryKeyLength(encrypted, i, 'e');
+            VigenereCipher cip = new VigenereCipher(key);
+            String decrypted = cip.decrypt(encrypted);
+            int count = countWords(decrypted, dictionary);
+            keys.put(key, count);
+        }
+        int max = 0;
+    	int[] foundKey = null;
+    	
+    	for (int[] key : keys.keySet()) {
+    	    if (max < keys.get(key)) {
+    		max = keys.get(key);
+    		foundKey = key;
+            }
+    	}
+    	System.out.println(foundKey);
+    	VigenereCipher vc = new VigenereCipher(foundKey);
+    	System.out.println("Decrypted word count:" + countWords(vc.decrypt(encrypted), dictionary));
+    	return vc.decrypt(encrypted);
+    }
     public void breakVigenere () {
         String fr = new FileResource().asString();
-        int[] length = tryKeyLength(fr,5,'e');
-        VigenereCipher cip = new VigenereCipher(length);
-        String decrypted = cip.decrypt(fr);
+        FileResource dictionary =  new FileResource();
+        HashSet<String> dict =  readDictionary(dictionary);
+        String decrypted = breakForLanguage(fr,dict);
         System.out.println(decrypted);
     }
+    
+   
     
 }
